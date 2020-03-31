03fd0c8133df246cceb515b4de2a033d1041ff39
yanruchen36 <61774470+yanruchen36@users.noreply.github.com>
2020-03-31 18:13:26 +0800

    Add files via upload

---
+++ HW2/hw2ytm計算.py
@@ -0,0 +1,31 @@
+"""作業二
+計算市場債券報價中的ytm、spot rate、forward rate
+以及建立forward rate對照表"""
+
+# 首先取得所需值
+
+p = int(input('債券當期價格(元)=')) # 債券當前價格
+f = int(input('債券票面價值(元)=')) # 債券票面價值
+c = 0.01*int(input('債券票面利率(％)=')) # 債券票面利率
+n = int(input('債券到期年數(年)=')) # 到期年數
+
+C = c*f
+
+
+for i in range(1,10000):
+
+	r = i/100 #換成百分率
+	F = f/((1+0.01*r)**n) #本金和
+	TC = 0 #利息和
+
+	for j in range(1,n+1):
+		TC += C/((1+0.01*r)**j)
+	
+	pv = TC + F #present value
+
+	d = pv - p # 因為r越大的話pv會慢慢變小
+	if d <= 0:
+		ytm = r
+		print('ytm=',r,'%')
+		break
+
